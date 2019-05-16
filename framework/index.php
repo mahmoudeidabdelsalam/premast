87443f02dfc9d4663ed68a5c4cfc95c297a860d4
Mahmoud Eid <mahmoud.eid@code95.com>
2019-05-16 20:36:38 +0200

    #PREMAST #time 10m #comment edit style image fit and footer #ADD

--- framework/index.php
+++ framework/index.php
@@ -20,11 +20,8 @@
    include $filename;
  }
 
- if ( (isset($_GET['action']) && $_GET['action'] != 'logout') || (isset($_POST['login_location']) && !empty($_POST['login_location'])) ) {
-    add_filter('login_redirect', 'my_login_redirect', 10, 3);
-    function my_login_redirect() {
-        $location = $_SERVER['HTTP_REFERER'];
-        wp_safe_redirect($location);
-        exit();
-    }
+add_action('wp_logout','auto_redirect_after_logout');
+function auto_redirect_after_logout(){
+  wp_redirect( home_url() );
+  exit();
 }
