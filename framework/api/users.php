20a15075f7c6bfc988a9587d66dcdc9cea2a83c7
Mahmoud <mahmoud@eramint.com>
2019-07-27 13:26:00 +0200

    #PREMAST #time 2H #comment edit api #ADD

--- framework/api/users.php
+++ framework/api/users.php
@@ -3,43 +3,63 @@ function all_users($data){
 
   $data=$data->get_params('POST');
   extract($data);
-  $page = !empty($page) ? $page : 1;
-  $total_users = count_users();
-  $total_users = $total_users['total_users'];
-  $number = 1; // ie. 20 users page page 
+
+  $email = !empty($email) ? $email : false;
+  $password = !empty($password) ? $password : false;
 
   $args = array(
     'count_total'  => false,
     'fields'       => 'all',
-    'offset' => $page ? ($page - 1) * $number : 0,
-    'number' => $number,
   ); 
   
   $users = get_users( $args );
 
-  if($users){
-    $users_data = [];
-    foreach ($users as $post) {
-      $users_data[] = [
-        'email' => $post->user_email,
-        'password' => $post->user_pass,
-      ];
+  if($email && $password){
+
+    $emails = [];
+    $passwords = [];
+
+    foreach ($users as $user) {
+      $passwords[] = $user->user_pass;
+      $emails[] =  $user->user_email;
     }
 
+    $user = get_user_by( 'email', $email );
+    $userId = $user->ID;
+    $user_meta = get_userdata($userId);
+    $user_roles = $user_meta->roles;
 
+    if(in_array("administrator", $user_roles)){
+      $admin = true;
+    } else {
+      $admin = false;
+    }
+
+    if (in_array($email, $emails) && wp_check_password( $password, $user->data->user_pass, $user->ID)) { 
+      $login = true;
+      $message = 'successfully Login';
+    } else { 
+      $login = false;
+      $message = 'Try to login again (login or password) error';
+    } 
+
+    $array =  [
+      'IsSuccess' => $login,
+      'IsAdmin' => $admin
+    ];
 
     $result = [
       'success' => true,
       'code' => 200,
-      'message' => 'successfully retrieved',
-      'data' => $users_data,
+      'message' => $message,
+      'data' => $array,
     ];
     return $result;
   } else {
     $result = [
-      'success' => 'false',
+      'success' => false,
       'code' => 404,
-      'message' => 'no User found',
+      'message' => 'login or password not fund',
     ];
     return $result;
   }
@@ -47,14 +67,19 @@ function all_users($data){
 
 add_action('rest_api_init' , function(){
   register_rest_route('wp/api/' ,'users/login',array(
-    'methods' => 'GET',
+    'methods' => 'POST',
     'callback' => 'all_users',
     'args' => array(
-      'page' => array(
+      'email' => array(
         'validate_callback' => function($param,$request,$key){
-          return is_numeric($param);
+          return true;
         }
       ),
+      'password' => array(
+        'validate_callback' => function($param,$request,$key){
+          return true;
+        }
+      ),      
     )
   ));
 });
