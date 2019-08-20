9a4b6e44eb3a259e0dc416b5d46dd682c571ffa2
Mahmoud <mahmoud@eramint.com>
2019-08-20 02:33:40 +0200

    #PREMAST #time 4H #comment Add page plans #ADD

--- resources/views/template-plan.blade.php
+++ resources/views/template-plan.blade.php
@@ -51,7 +51,7 @@
           @if( have_rows('plan_pricing_box') )
             @while ( have_rows('plan_pricing_box') ) @php the_row() @endphp
               <div class="col-md-3 col-sm-6 col-12">
-                <div class="box-pricing">
+                <div class="box-pricing @if( get_sub_field('check_best_plan') ) the-best @endif">
                   <div class="headline-pricing">
                     {{ the_sub_field('headline_plan_box') }}
                   </div>
@@ -77,12 +77,14 @@
                     @php 
                     $product_id = get_sub_field('choose_plan');  
                     @endphp
-
                       <div class="choose plan">
                         {!! do_shortcode( '[ajax_add_to_cart id="'.$product_id.'" text="choose plan" style="" show_price="false" /]' ) !!}
                       </div>
-                     
                   </div>
+
+                  @if( get_sub_field('check_best_plan') )
+                    <p class="best-plane">{{ _e('Best plane -', 'permast') }} <strong>{{ the_sub_field('text_best_plan') }}</strong></p>
+                  @endif
                 </div>
               </div>
             @endwhile
@@ -94,6 +96,55 @@
         </div>
       </div>
     </section>
+    
+    @if( have_rows('featured_item') )
+      <section class="featured-item bg-white pt-5 pb-5">
+        <div class="container">
+          <div class="row align-items-center text-center justify-content-center">
+            @while ( have_rows('featured_item') ) @php the_row() @endphp
+              <div class="col-md-3 col-sm-6 col-12">
+                <div class="box-featured">
+                  <img src="{{ the_sub_field('icon_featured_item') }}" alt="{{ the_sub_field('headline_featured_item') }}">
+                  <h3>{{ the_sub_field('headline_featured_item') }}</h3>
+                  <p>{{ the_sub_field('Instructions_featured_item') }}</p>
+                </div>
+              </div>
+            @endwhile
+          </div>
+        </div>          
+      </section>
+    @endif
+
+    @if( have_rows('faq_plans') )
+      <section class="faq-item pt-5 pb-5">
+        <div class="container">
+          <div class="row">
+            <h3 class="headline-faq">{{ _e('FAQ', 'permast') }}</h3>
+            <p class="tag-faq">{{ _e('Here a some of our frequently asked questions updated frequently to answer all your requests.', 'permast') }}</p>
+            <div class="accordion col-12 p-0" id="accordionExample">
+              @while ( have_rows('faq_plans') ) @php the_row() @endphp
+              
+                <div class="card col-12 p-0">
+                  <div class="card-header" id="heading-{{ get_row_index() }}">
+                    <a class="collapse-btn col-12 p-0" data-toggle="collapse" data-target="#collapse-{{ get_row_index() }}" aria-expanded="true" aria-controls="collapseOne">
+                      {{ the_sub_field('ask_plan') }} <i class="fa fa-angle-down" aria-hidden="true"></i>
+                    </a>
+                  </div>
+
+                  <div id="collapse-{{ get_row_index() }}" class="collapse" aria-labelledby="heading-{{ get_row_index() }}" data-parent="#accordionExample">
+                    <div class="card-body">
+                      {{ the_sub_field('question_plan') }}
+                    </div>
+                  </div>
+                </div>
+
+              @endwhile
+            </div>
+            <p class="copy-faq">Can’t find your question here? <span>Ask a question</span></p>
+          </div>
+        </div>          
+      </section>
+    @endif
   @endwhile
 @endsection
 
