282de69c0f263ffa08208f18a1d8f6bcb29cdd91
Mahmoud <mahmoud@eramint.com>
2019-08-26 01:04:56 +0200

    #PREMAST #time 4H #comment edit limit and count #EDIT

--- resources/views/partials/content-single-product.blade.php
+++ resources/views/partials/content-single-product.blade.php
@@ -14,14 +14,18 @@ global $product;
 
   $limit = somdn_has_user_reached_limit(get_the_ID(), $current_user->ID);
 
-  if ($limit)  {
+  $limit_membership = wc_memberships_get_user_active_memberships($current_user->ID);
+
+  // dd($limit);
+
+  if ($limit && !$limit_membership)  {
     if(get_field('link_limit', 'option')) {
       wp_redirect( get_field('link_limit', 'option') );
       exit;
     }
   } 
   
-  $is_memberships = wc_memberships_get_user_active_memberships($current_user->ID);
+
   
   $author = get_the_author_meta('ID');
 
