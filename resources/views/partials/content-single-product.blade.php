75d5e3c04b5af68d56ee5501bfb6b3c4a1982a13
Mahmoud <mahmoud@eramint.com>
2019-09-03 15:02:38 +0200

    #PREMAST #time 4H #comment Add permast  #EDIT

--- resources/views/partials/content-single-product.blade.php
+++ resources/views/partials/content-single-product.blade.php
@@ -109,7 +109,7 @@ global $product;
             $time = get_the_time('Y-m-d');
           @endphp
           <div class="custom-summary">            
-            @if ($limit && !$limit_membership && !$sale && !$price || $count_download == $limits_amount) 
+            @if ($limit && !$limit_membership && !$sale && !$price || $count_download == $limits_amount && !$sale && !$price) 
               @if(get_field('link_limit', 'option'))
                 <div class="bottom-summary col-12 mt-4 mb-4 w-100">
                   <a class="btn-limit" href="{{ get_field('link_limit', 'option') }}" id="somdn-form-submit-button">{{ _e('Download Now', 'premast') }}</a>  
