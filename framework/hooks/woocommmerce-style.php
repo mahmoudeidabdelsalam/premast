d13611b48ec51a4b91ec2c07b623070c18f63ccb
Mahmoud <mahmoud@eramint.com>
2019-09-29 19:56:11 +0200

    #PREMAST #time 9H #comment Add chekout box custon and edit h1 single  #EDIT

--- framework/hooks/woocommmerce-style.php
+++ framework/hooks/woocommmerce-style.php
@@ -252,3 +252,32 @@ function mwe_get_formatted_shipping_name_and_address($user_id) {
 }
 
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+
+
+
+add_action( 'woocommerce_email', 'remove_email_order_downloads', 10, 1 );
+function remove_email_order_downloads( $emails ){
+    remove_action( 'woocommerce_email_order_details', array( $emails, 'order_downloads' ), 10 );
+    add_action( 'woocommerce_email_order_details', 'custom_order_downloads', 9, 4 );
+}
+
+function custom_order_downloads( $order, $sent_to_admin = false, $plain_text = false, $email = '' ) {
+    $show_downloads = $order->has_downloadable_item() && $order->is_download_permitted() && ! $sent_to_admin && $order->has_status('completed');
+
+    if ( ! $show_downloads ) {
+        return;
+    }
+
+    $downloads = $order->get_downloadable_items();
+    $columns   = apply_filters( 'woocommerce_email_downloads_columns', array(
+        'download-product' => __( 'Product', 'woocommerce' ),
+        'download-expires' => __( 'Expires', 'woocommerce' ),
+        'download-file'    => __( 'Download', 'woocommerce' ),
+    ) );
+
+    if ( $plain_text ) {
+        wc_get_template( 'emails/plain/email-downloads.php', array( 'order' => $order, 'sent_to_admin' => $sent_to_admin, 'plain_text' => $plain_text, 'email' => $email, 'downloads' => $downloads, 'columns' => $columns ) );
+    } else {
+        wc_get_template( 'emails/email-downloads.php', array( 'order' => $order, 'sent_to_admin' => $sent_to_admin, 'plain_text' => $plain_text, 'email' => $email, 'downloads' => $downloads, 'columns' => $columns ) );
+    }
+}
