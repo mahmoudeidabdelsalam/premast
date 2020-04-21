40149853f5f137c7e369f636344a185194291ab7
Mahmoud <mahmoud@eramint.com>
2020-04-21 11:42:32 +0200

    add paddle page

--- framework/post-type/paddle.php
+++ framework/post-type/paddle.php
@@ -29,7 +29,7 @@ function paddles_post_type() {
         'menu_icon' => 'dashicons-welcome-widgets-menus',
         'can_export' => true,
         'has_archive' => true,
-        'exclude_from_search' => false,
+        'exclude_from_search' => true,
         'publicly_queryable' => true,
         'capability_type' => 'page',
         'show_in_rest' => true,
