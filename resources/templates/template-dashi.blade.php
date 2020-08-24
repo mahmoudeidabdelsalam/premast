c8719ee70c2cc28c95287521e22b78de1594eae5
Menna Medhat <mnnmdht@gmail.com>
2020-08-24 14:04:52 +0200

    fix issues

--- resources/templates/template-dashi.blade.php
+++ resources/templates/template-dashi.blade.php
@@ -496,6 +496,20 @@ h5.color_title {
 }
 
     </style>
+    <script>
+      window.productHuntUpcoming = {
+        appId: 94791,
+        position: 'bottomLeft',
+      };
+
+      (function(doc, scr, src, a, b) {
+        a = doc.createElement(scr);
+        b = doc.getElementsByTagName(scr)[0];
+        a.async = true;
+        a.src = src;
+        b.parentNode.insertBefore(a, b);
+      })(document, 'script', 'https://assets.producthunt.com/assets/upwigloader.js');
+    </script>
 
   @endwhile
 @endsection
