f60c4eccb91d96a511685a5d38e0dcc799fab174
Mahmoud <mahmoud@eramint.com>
2020-09-07 16:27:56 +0200

    edit dashi download demo

--- resources/templates/template-dashi.blade.php
+++ resources/templates/template-dashi.blade.php
@@ -87,7 +87,7 @@ foreach ($downloads as $download) {
                         </button>
 
                         <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade download-dashi-demo" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                           <div class="modal-dialog modal-dialog-centered">
                             <div class="modal-content">
                               <div class="modal-header">
