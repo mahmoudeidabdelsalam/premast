6fb83662b08bdd5a23711b739922fd97f7a59c90
Mahmoud <mahmoud@eramint.com>
2020-09-10 15:20:53 +0200

    add login

--- resources/templates/template-signin.blade.php
+++ resources/templates/template-signin.blade.php
@@ -26,9 +26,9 @@
     <div class="row justify-content-left">
       <div class="col-md-5 p-0">
         <div class="background-img">
-          <div class="headline text-center">
-            <h2 class="welcome">Welcome Back to Premast</h2>
-            <h5 class="subline">Download your preferred design from huge collection <br>of professionally, creative designed powerpoint <br> templates for all your needs.</h5>
+          <div class="WelcomeText">
+            <h2 class="HeadLineWelcome text-left">Welcome Back to Premast</h2>
+            <h5 class="SubHeadLineWelcome text-left">Download your preferred design from huge collection <br>of professionally, creative designed powerpoint <br> templates for all your needs.</h5>
           </div>
         </div>
       </div>
@@ -37,6 +37,7 @@
       <div class="col-md-7 " style="padding:5.5rem;">
         <h2 class="access">Access your account</h2>
         <div class="sign-in">
+          <div class="formLogin tab-switch active " id="LoginForm">
             @php
                 $args = array(
                 'echo'           => true,
@@ -56,7 +57,11 @@
 
             {{ wp_login_form($args) }}
 
+            <a href="#" class="switch-link" data-tab="LostPassword">Lost your password?</a>
 
+          </div>
+
+          <div class="formLogin tab-switch" id="LostPassword">
             <?php
             defined( 'ABSPATH' ) || exit;
             do_action( 'woocommerce_before_lost_password_form' );
@@ -77,6 +82,8 @@
             </form>
             <?php do_action( 'woocommerce_after_lost_password_form' ); ?>
 
+            <a href="#" class="switch-link" data-tab="LoginForm">Login</a>
+          </div>
         </div>
       </div>
     </div>
@@ -91,10 +98,8 @@
       // Tabs Custom
       $('.switch-link').click(function(){
         var tab_id = $(this).attr('data-tab');
-        $('.tab-content .tab-pane').removeClass('show');
-        $('.tab-content .tab-pane').removeClass('active');
+        $('.tab-switch').removeClass('active');
         $("#"+tab_id).addClass('active');
-        $("#"+tab_id).addClass('show');
       });
 
     });
@@ -109,128 +114,146 @@
 
 
 <style>
-.background-img{
-  height:600px;
-  width: auto;
-  background-repeat: no-repeat;
-  background-size:cover;
-
-}
-h2.welcome{
-font-family:'Roboto' , sans-serif;
-font-style: normal;
-font-weight: bold;
-font-size: 30px;
-line-height: 36px;
-color: #F9FAFB;
+.background-img {
+    height: 600px;
+    width: auto;
+    background-repeat: no-repeat;
+    background-size: cover;
 }
 
-h5.subline{
-font-family:'Roboto' sans-serif;
-font-style: normal;
-font-weight: normal;
-font-size: 16px;
-line-height: 24px;
-letter-spacing: 0.04px;
-color: #FFFFFF;
-text-align: center;
-align-items: center;
+h2.HeadLineWelcome {
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 36px;
+    color: #F9FAFB;
+}
+
+h5.SubHeadLineWelcome {
+    font-family: 'Roboto'sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: 0.04px;
+    color: #FFFFFF;
+    text-align: center;
+    align-items: center;
 
 }
-.headline{
-  padding-top: 15.5rem;
-  text-align: center;
-  align-items: center;
+
+.WelcomeText {
+  display: flex;
+      padding-left: 20%;
+  justify-content: center;
+  flex-flow: column;
+  height: 100%;
 }
+
 h2.access {
-font-family:'Roboto';
-font-style: normal;
-font-weight: bold;
-font-size: 30px;
-line-height: 36px;
-color: #282F39;
-opacity: 0.82;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 36px;
+    color: #282F39;
+    opacity: 0.82;
 }
+
 .sign-in {
-  padding-top: 30px;
+    padding-top: 30px;
 }
+
 .form-control {
-  border-radius: 8px!important;
-  background: #FFFFFF;
-  border: 1px solid #E3E3E3!important;
-  box-sizing: border-box;
-  padding: 1.2rem .95rem!important;
+    border-radius: 8px !important;
+    background: #FFFFFF;
+    border: 1px solid #E3E3E3 !important;
+    box-sizing: border-box;
+    padding: 1.2rem .95rem !important;
 
 }
-label{
-font-family: 'Roboto' sans-serif;
-font-style: normal;
-font-weight: normal;
-font-size: 14px;
-line-height: 21px;
-align-items: center;
-letter-spacing: 0.04px;
-color: #646464;
+
+label {
+    font-family: 'Roboto'sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 21px;
+    align-items: center;
+    letter-spacing: 0.04px;
+    color: #646464;
 }
-small#forgotpassword{
-  font-family: Roboto;
-font-style: normal;
-font-weight: normal;
-font-size: 14px;
-line-height: 21px;
-text-align: right;
-letter-spacing: 0.04px;
-color: #1E6DFB!important;
-opacity: 0.88;
-padding-top: 15px;
+
+small#forgotpassword {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 21px;
+    text-align: right;
+    letter-spacing: 0.04px;
+    color: #1E6DFB !important;
+    opacity: 0.88;
+    padding-top: 15px;
 }
-a.btn.btn-primary {
-background: linear-gradient(134.71deg, #6B73FF -0.5%, #000DFF 100%);
-border-radius: 30px;
-padding: 8px 100px 8px 100px;
-border-color:none!important;
-margin-top: 15px;
+
+.btn.btn-primary {
+    background: linear-gradient(134.71deg, #6B73FF -0.5%, #000DFF 100%);
+    border-radius: 30px;
+    padding: 8px 100px 8px 100px;
+    border-color: none !important;
+    margin-top: 15px;
 }
-a.btn.btn-primary:hover{
-  background: linear-gradient(134.71deg, #000DFF -0.5%,  #6B73FF 100%);
+
+.btn.btn-primary:hover {
+    background: linear-gradient(134.71deg, #000DFF -0.5%, #6B73FF 100%);
 }
-a.btn.btn-outline-dark {
-  padding: 8px 60px 8px 60px;
-  border-radius: 30px;
-  margin:15px 0px 0px 15px;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 19px;
-  letter-spacing: 0.116946px;
-  text-transform: capitalize;
-  color: #3F4A59;
+
+.btn.btn-outline-dark {
+    padding: 8px 60px 8px 60px;
+    border-radius: 30px;
+    margin: 15px 0px 0px 15px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 19px;
+    letter-spacing: 0.116946px;
+    text-transform: capitalize;
+    color: #3F4A59;
 }
-a.btn.btn-outline-dark:hover{
-  color:#FFFFFF;
+
+.btn.btn-outline-dark:hover {
+    color: #FFFFFF;
 }
-a.btn.btn-outline-dark::before{
-  font-family: 'fontawesome'; font-weight: 500; content: "\f1a0";
-  padding-right: 7px;
-  font-size: 16px;
+
+.btn.btn-outline-dark::before {
+    font-family: 'fontawesome';
+    font-weight: 500;
+    content: "\f1a0";
+    padding-right: 7px;
+    font-size: 16px;
 }
-/* media query  */
+
 @media only screen and (max-width: 600px) {
-  a.btn.btn-outline-dark {
-    padding:8px;
-    width:100%;
-    margin:15px 0px 0px 0px;
-  }
-  a.btn.btn-primary {
-    padding:8px;
-    width:100%;
-  }
+    a.btn.btn-outline-dark {
+        padding: 8px;
+        width: 100%;
+        margin: 15px 0px 0px 0px;
+    }
 
-
-
-
-
+    a.btn.btn-primary {
+        padding: 8px;
+        width: 100%;
+    }
 }
 
+.tab-switch {
+  display: none;
+}
+
+.tab-switch.active {
+    display: block;
+}
 </style>
